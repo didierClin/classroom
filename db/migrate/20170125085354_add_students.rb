894921a85fe99c1949f74ef055c9bf029d095bad
Pierre Merlin <Pierre Merlin>
2017-01-25 10:26:02 +0100

    add new student

--- db/migrate/20170125085354_add_students.rb
+++ db/migrate/20170125085354_add_students.rb
@@ -1,5 +1,7 @@
 class AddStudents < ActiveRecord::Migration[5.0]
   def change
-    create_table :students
+    create_table :students do |table|
+      table.column :name, :string
+    end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,7 @@
 ActiveRecord::Schema.define(version: 20170125085354) do
 
   create_table "students", force: :cascade do |t|
+    t.string "name"
   end
 
 end
