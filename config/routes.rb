91e519e89d86079ccab442c418e720bdf20e4c7f
didierclin <didier.c62@numericable.fr>
2017-01-26 12:37:36 +0100

    correct routing to students

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,15 @@
 Rails.application.routes.draw do
+
+  root to: 'courses#index'
   resources :courses, only: [:index, :show]
   # get 'courses/index'
   #
   # get 'courses/show/:id', to: 'courses#show'
-
-  resources :students, only: [:index, :show]
-  # get 'students/index'
+  # resources :students, only: [:index, :show]
+  get 'students/index', to: 'students#index', as: 'students'
   #
   # get 'students/show/:id', to: "students#show"
+  get '/student/:id', to: 'students#show', as: 'student'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
