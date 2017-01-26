00eb6a11a5e1aad007783c36063dfe7f967a480c
didierclin <didier.c62@numericable.fr>
2017-01-26 10:54:07 +0100

    change route to show id

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'students/index'
 
-  get 'students/show'
+  get 'students/show/:id', to: "students#show"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
