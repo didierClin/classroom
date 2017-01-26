80e86b7ba9f93200e8fd7d067090f1de6f3dd789
didierclin <didier.c62@numericable.fr>
2017-01-26 11:26:01 +0100

    change routes with resources entries

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
-  get 'students/index'
+  resources :courses, only: [:index, :show]
+  # get 'courses/index'
+  #
+  # get 'courses/show/:id', to: 'courses#show'
 
-  get 'students/show/:id', to: "students#show"
+  resources :students, only: [:index, :show]
+  # get 'students/index'
+  #
+  # get 'students/show/:id', to: "students#show"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
