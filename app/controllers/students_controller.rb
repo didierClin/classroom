5116f8de718cde66f2f233071ee3b269b5ba76bc
didierclin <didier.c62@numericable.fr>
2017-01-30 16:48:08 +0100

    add header layout partial - add new student method

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -8,4 +8,8 @@ class StudentsController < ApplicationController
   def show
     @student = Student.find(params[:id])
   end
+
+  def new
+    @student = Student.new
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -15,11 +15,11 @@ Rails.application.routes.draw do
   # get 'courses/index'
   #
   # get 'courses/show/:id', to: 'courses#show'
-  # resources :students, only: [:index, :show]
-  get 'students/index', to: 'students#index', as: 'students'
-  #
-  # get 'students/show/:id', to: "students#show"
-  get '/student/:id', to: 'students#show', as: 'student'
+  resources :students, only: [:index, :show, :new]
+  # get 'students/index', to: 'students#index', as: 'students'
+  # #
+  # # get 'students/show/:id', to: "students#show"
+  # get '/student/:id', to: 'students#show', as: 'student'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
