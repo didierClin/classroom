2f2ad9f8f12bc7d9ced679e1f7ed864ebc94d80f
didierclin <didier.c62@numericable.fr>
2017-02-01 11:59:44 +0100

    add action new, create, update in ctrl courses

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,10 +1,30 @@
 class CoursesController < ApplicationController
+    before_action :set_course, only: [:show, :update]
   def index
     @courses = Course.all
   end
 
   def show
-    @course = Course.find(params[:id])
     # fail
+  end
+
+  def new
+  end
+
+  def create
+
+  end
+
+  def update
+  end
+
+  private
+
+  def set_course
+    @course = Course.find(params[:id])
+  end
+
+  def course_params
+    params.require(:course).permit(:field, :teacher)
   end
 end
